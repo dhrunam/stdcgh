4c9d017cbf2eb6e040f4e13082af52c871321938
Raja Siddharth Raju <comrade@Rajas-MacBook-Pro.local>
2023-03-15 08:15:16 +0530

    Checking

--- hcgms_frontend/src/app/dashboard/masters/room-category-master/room-category-master.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-room-category-master',
-  templateUrl: './room-category-master.component.html',
-  styleUrls: ['./room-category-master.component.css']
-})
-export class RoomCategoryMasterComponent {
-}
